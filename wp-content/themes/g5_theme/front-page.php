f23532733c61167790b90a94078e8cfba5d0fc25
Rrrricky <eric.dufreche@hetic.net>
2018-10-31 21:32:27 +0100

    new scss variables

--- wp-content/themes/g5_theme/front-page.php
+++ wp-content/themes/g5_theme/front-page.php
@@ -1,9 +1,10 @@
 <?php get_header() ?>
 
 	<main id="home-page-template" class="page-template">
-		<h1>Découvrez les régions et espèces menacées</h1>
-		<h3>Le réchauffement climatique et ses effets sur la nature et les animaux</h3>
-		<button>Commencer –</button>
+		<h1 class="page-template__title">Découvrez les régions et espèces menacées</h1>
+		<div class="page-template__subtitle">Le réchauffement climatique et ses effets sur la nature et les animaux</div>
+		<button class="page-template__button">Commencer –</button>
+		<span class="page-template__catch">Ne subissons plus. Agissons !</span>
 	</main>
 
 <?php get_footer() ?>
