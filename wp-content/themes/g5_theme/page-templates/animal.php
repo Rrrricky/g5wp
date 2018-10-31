b224c5386fc37dba92611f71ce693638499ae516
amerre <adr.merre@gmail.com>
2018-10-31 23:40:24 +0100

    Grid-menu front created

--- wp-content/themes/g5_theme/page-templates/animal.php
+++ wp-content/themes/g5_theme/page-templates/animal.php
@@ -9,7 +9,44 @@ Template Name: Animal template
 ?>
 
 <?php get_header() ?>
-	<main id="animal-page-template" class="page-template">
-		<div>Animal</div>
+	<main id="animal-page-template" class="page-template grid-menu">
+
+		<div class="grid-menu__items">
+			<div class="grid-menu__number">1</div>
+			<div class="grid-menu__name">Elephants</div>
+			<img src="https://placeimg.com/418/284/animals">
+		</div>
+		<div class="grid-menu__items">
+			<div class="grid-menu__number">2</div>
+			<div class="grid-menu__name">Monkey</div>
+			<img src="https://placeimg.com/418/284/arch">
+		</div>
+		<div class="grid-menu__items">
+			<div class="grid-menu__number">3</div>
+			<div class="grid-menu__name">Lion</div>
+			<img src="https://placeimg.com/418/284/nature">
+		</div>
+		<div class="grid-menu__items">
+			<div class="grid-menu__number">4</div>
+			<div class="grid-menu__name">Dolphin</div>
+			<img src="https://placeimg.com/418/284/people">
+		</div>
+		<div class="grid-menu__items">
+			<div class="grid-menu__number">5</div>
+			<div class="grid-menu__name">Gorilla</div>
+			<img src="https://placeimg.com/418/284/tech">
+		</div>
+		<div class="grid-menu__items">
+			<div class="grid-menu__number">6</div>
+			<div class="grid-menu__name">Shark</div>
+			<img src="https://placeimg.com/418/284/tech/grayscale">
+		</div>
+		<div class="grid-menu__items">
+			<div class="grid-menu__number">7</div>
+			<div class="grid-menu__name">Shark</div>
+			<img src="https://placeimg.com/418/284/tech/sepia">
+		</div>
+
+
 	</main>
 <?php get_footer() ?>
