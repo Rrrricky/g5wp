0d82c24d38843279923f75be26df191ae1af9109
Rrrricky <eric.dufreche@hetic.net>
2018-10-31 21:58:48 +0100

    fix menu css

--- wp-content/themes/g5_theme/front-page.php
+++ wp-content/themes/g5_theme/front-page.php
@@ -1,10 +1,10 @@
 <?php get_header() ?>
 
-	<main id="home-page-template" class="page-template">
-		<h1 class="page-template__title">Découvrez les régions et espèces menacées</h1>
-		<div class="page-template__subtitle">Le réchauffement climatique et ses effets sur la nature et les animaux</div>
-		<button class="page-template__button">Commencer –</button>
-		<span class="page-template__catch">Ne subissons plus. Agissons !</span>
+	<main id="home-page-template" class="custom-home-page">
+		<h1 class="custom-home-page__title">Découvrez les régions et espèces menacées</h1>
+		<div class="custom-home-page__subtitle">Le réchauffement climatique et ses effets sur la nature et les animaux</div>
+		<button class="custom-home-page__button">Commencer –</button>
+		<span class="custom-home-page__catch">Ne subissons plus. Agissons !</span>
 	</main>
 
 <?php get_footer() ?>
