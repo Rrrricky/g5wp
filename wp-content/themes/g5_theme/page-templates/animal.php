fe0afad1edeaedc0d5fb67583db030e91602c333
amerre <adr.merre@gmail.com>
2018-11-05 22:17:44 +0100

    Temporar fix for grid on chrome, adjust on name placement

--- wp-content/themes/g5_theme/page-templates/animal.php
+++ wp-content/themes/g5_theme/page-templates/animal.php
@@ -20,32 +20,23 @@ Template Name: Animal template
 			<div class="grid-menu__number"></div>
 			<div class="grid-menu__name">Monkey</div>
 			<img src="https://placeimg.com/418/284/animals">
+
 		</div>
 		<div class="grid-menu__items">
 			<div class="grid-menu__number"></div>
 			<div class="grid-menu__name">Lion</div>
+
 			<img src="https://placeimg.com/418/284/animals">
+
 		</div>
 		<div class="grid-menu__items">
 			<div class="grid-menu__number"></div>
 			<div class="grid-menu__name">Dolphin</div>
+
 			<img src="https://placeimg.com/418/284/animals">
+
 		</div>
-		<div class="grid-menu__items">
-			<div class="grid-menu__number"></div>
-			<div class="grid-menu__name">Gorilla</div>
-			<img src="https://placeimg.com/418/284/animals">
-		</div>
-		<div class="grid-menu__items">
-			<div class="grid-menu__number"></div>
-			<div class="grid-menu__name">Shark</div>
-			<img src="https://placeimg.com/418/284/animals">
-		</div>
-		<div class="grid-menu__items">
-			<div class="grid-menu__number"></div>
-			<div class="grid-menu__name">Shark</div>
-			<img src="https://placeimg.com/418/284/animals">
-		</div>
+
 
 
 	</main>
