eacc9dd423ce69439b35f765408e2739bff197a1
amerre <adr.merre@gmail.com>
2018-11-06 15:20:34 +0100

    Adding link on each animals/regions using single.php

--- wp-content/themes/g5_theme/single.php
+++ wp-content/themes/g5_theme/single.php
@@ -1,1 +1,8 @@
-<h1>hell</h1>
+<?php get_header() ?>
+
+
+<p style="font-size:40px">This is the "<?php the_title(); ?>" page, using the single.php template</p>
+
+<?php the_post_thumbnail( 'menu-thumb' ); ?>
+
+<?php get_footer() ?>
