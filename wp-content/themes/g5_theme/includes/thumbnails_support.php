d2116108271dd73e9818e5629ab24fdc84c3941e
amerre <adr.merre@gmail.com>
2018-11-09 12:56:49 +0100

    Starting to design single animal's page. Back to bg color black. Redefine thumb size

--- wp-content/themes/g5_theme/includes/thumbnails_support.php
+++ wp-content/themes/g5_theme/includes/thumbnails_support.php
@@ -7,7 +7,7 @@
 
 function thumbnails_theme_support(){
   add_theme_support('post-thumbnails');
-  add_image_size('page-thumb', 800, 1133, true);
+  add_image_size('page-thumb', 702, 824, false);
   add_image_size('home-thumb', 600, 9999, false);
   add_image_size('menu-thumb', 418, 284, true);
 }
--- wp-content/themes/g5_theme/single-animal.php
+++ wp-content/themes/g5_theme/single-animal.php
@@ -1,8 +1,31 @@
 <?php get_header() ?>
 
 
-<p style="font-size:40px">This is the "<?php the_title(); ?>" page, using the single-animal.php template</p>
+<div class="animal-wrapper">
 
-<?php the_post_thumbnail( 'menu-thumb' ); ?>
+  <div class="animal-wrapper__img">
+    <?php the_post_thumbnail( 'page-thumb' ); ?>
+  </div>
+
+  <div class="animal-wrapper__text" style="font-size:40px">
+    <p class="animal-wrapper__status">
+      Espece
+      <span class="line"></span>
+    </p>
+    <p class="animal-wrapper__title">
+      <?php the_title(); ?>
+    </p>
+    <p class="animal-wrapper__status">
+      Status
+      <span class="line"></span>
+    </p>
+    <p class="animal-wrapper__title__smaller">
+      En Danger
+    </p>
+  </div>
+
+</div>
+
+
 
 <?php get_footer() ?>
