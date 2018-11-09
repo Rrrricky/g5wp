61bfe3330af02271e56e9478b5628b7c030978e9
amerre <adr.merre@gmail.com>
2018-11-09 19:03:21 +0100

    Automatization of animal page

--- wp-content/themes/g5_theme/single-animal.php
+++ wp-content/themes/g5_theme/single-animal.php
@@ -1,26 +1,34 @@
+
+<!-- Get the image to display -->
+<?php
+$image = get_field('first_image');
+$size = 'full'; // (thumbnail, medium, large, full or custom size)
+?>
+<!-- ------------------------ -->
+
 <?php get_header() ?>
 
 
 <div class="animal-wrapper">
 
   <div class="animal-wrapper__img">
-    <?php the_post_thumbnail( 'page-thumb' ); ?>
+    <?php echo wp_get_attachment_image( $image, $size ); ?>
   </div>
 
   <div class="animal-wrapper__text" style="font-size:40px">
-    <p class="animal-wrapper__status">
+    <p class="animal-wrapper__statut">
       Espece
       <span class="line"></span>
     </p>
     <p class="animal-wrapper__title">
-      <?php the_title(); ?>
+      <?php the_field('espece'); ?>
     </p>
-    <p class="animal-wrapper__status">
+    <p class="animal-wrapper__statut">
       Status
       <span class="line"></span>
     </p>
     <p class="animal-wrapper__title__smaller">
-      En Danger
+      <?php the_field('statut'); ?>
     </p>
   </div>
 
