ed0338758b5e7cc56223849f0740132657df0fdf
amerre <adr.merre@gmail.com>
2018-11-09 11:29:21 +0100

    Created singular page for animal/region

---
+++ wp-content/themes/g5_theme/single-animal.php
@@ -0,0 +1,8 @@
+<?php get_header() ?>
+
+
+<p style="font-size:40px">This is the "<?php the_title(); ?>" page, using the single-animal.php template</p>
+
+<?php the_post_thumbnail( 'menu-thumb' ); ?>
+
+<?php get_footer() ?>
