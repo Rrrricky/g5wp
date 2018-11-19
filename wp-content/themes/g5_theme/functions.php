b490a09b900c88b032ab283777d33f7a72a59235
Rrrricky <eric.dufreche@hetic.net>
2018-11-19 09:56:27 +0100

    Try to remove script type

--- wp-content/themes/g5_theme/functions.php
+++ wp-content/themes/g5_theme/functions.php
@@ -14,3 +14,10 @@ define( 'ADMIN_IMAGES_URL' ,    IMAGES_URL .   '/admin'             );
 foreach ( glob( THEME_PATH . "/includes/*.php" ) as $file ) {
   include_once $file;
 }
+
+
+add_filter('style_loader_tag', 'codeless_remove_type_attr', 10, 2);
+add_filter('script_loader_tag', 'codeless_remove_type_attr', 10, 2);
+function codeless_remove_type_attr($tag, $handle) {
+    return preg_replace( "/type=['\"]text\/(javascript|css)['\"]/", '', $tag );
+}
